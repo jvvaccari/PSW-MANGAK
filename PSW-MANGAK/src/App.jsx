8d70aec82bb215cb7949df7c2946553c9c1ddc17
jvvaccari <jv_vaccari@live.com>
2024-11-02 14:50:36 -0300

    My first commit

---
+++ PSW-MANGAK/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Vite + MUI!</h1>
+      <Button variant="contained" color="primary">
+        Botão MUI
+      </Button>
+    </div>
+  );
+}
+
+export default App;
