d74e0aed8124cb765eda1a974c8d6be96714c95f
João Vaccari <jvvaccari1@gmail.com>
2024-11-07 11:13:52 -0300

    reorganizing folders and adding the pages folder

--- PSW-MANGAK/src/App.jsx
+++ PSW-MANGAK/src/App.jsx
@@ -1,84 +1,15 @@
-import Actions from "./components/Actions";
-import ArtGallery from "./components/ArtGallery";
-import Content from "./components/Content";
-import Description from "./components/Description";
-import Header from "./components/Header";
-import TagsSection from "./components/TagsSection";
-import { Box } from "@mui/material";
-import chainsawCover from "./assets/img/Chainsaw_Man_Cover_Volume_1.svg";
-import img1 from "./assets/img/chainsaw1.jpg";
-import img2 from './assets/img/chainsaw2.jpg';
-import img3 from './assets/img/chainsaw3.jpeg';
-import img4 from './assets/img/chainsaw4.webp';
-import img5 from './assets/img/chainsaw5.jpg';
-import img6 from './assets/img/chainsaw6.jpeg';
-import img7 from './assets/img/chainsaw7.jpg';
-import img8 from './assets/img/chainsaw8.webp';
-import img9 from './assets/img/chainsaw9.webp';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MangaLandingPage from "./pages/MangaLandingPage";
+// import AnotherPage from "./pages/AnotherPage";
 
 function App() {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "flex-start",
-        minHeight: "100vh",
-        bgcolor: "#000",
-      }}
-    >
-      <Box
-        sx={{
-          width: "100%",          
-          bgcolor: "#000",
-          padding: "16px",        
-          color: "#fff",
-        }}
-      >
-
-        <Header/>
-
-        <Content
-          mangaImage={chainsawCover}
-          title="Chainsaw Man"
-          author="Tatsuki Fujimoto"
-          rating="9.30"
-          reviews="10k"
-          statusDot="•"
-          publication="PUBLICATION: 2021, ONGOING"
-        />
-
-        <Actions />
-
-        <Description
-          text="Broke young man + chainsaw demon = Chainsaw Man! Denji was a small-time devil hunter just trying to survive in a harsh world. After being killed on a job, he is revived by his pet devil-dog Pochita and becomes something new and dangerous—Chainsaw Man!"
-        />
-
-        {
-          [
-            {section: "Genres", tags: ["Action", "Adventure", "Fantasy"]},
-            {section: "Demographic", tags: ["Shounen", "Seinen", "Josei"]},
-            {section: "Buy", tags: ["Amazon", "Barnes & Noble", "Book Depository"]},
-            {section: "Track", tags: ["MyAnimeList", "AniList", "Kitsu"]}
-          ].map((data) => (
-            <TagsSection key={data.section} data={data} />
-          ))
-        }
-  
-        <ArtGallery imageList={[
-          img1.defaulr || img1,
-          img2.default || img2,
-          img3.default || img3,
-          img4.default || img4,
-          img5.default || img5,
-          img6.default || img6,
-          img7.default || img7,
-          img8.default || img8,
-          img9.default || img9,
-        ]}/>
-      
-      </Box>
-    </Box>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MangaLandingPage />} />
+        {/* <Route path="/another" element={<AnotherPage />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
--- PSW-MANGAK/src/components/Header.jsx
+++ PSW-MANGAK/src/components/Header.jsx
@@ -1,22 +1,35 @@
-import { Box, IconButton,Avatar } from "@mui/material";
+import { Box, IconButton, Avatar } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import styles from "./Header.module.css";
+import { useNavigate } from "react-router-dom";
 
-const Header = () => (
-  <Box className={styles.headerContainer}>
-    <IconButton color="inherit" sx={{width: "30px",height: "30px"}}>
-      <ArrowBackIcon />
-    </IconButton>
-    <Avatar 
-      sx={{
-        width: "30px",
-        height: "30px",
-        color: "#fff",
-        backgroundColor: "#000",
-        border: "2px solid #fff",
-      }}
-    />
-  </Box>
-);
+const Header = () => {
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+
+  return (
+    <Box className={styles.headerContainer}>
+      <IconButton
+        color="inherit"
+        onClick={handleBackClick}
+        sx={{ width: "30px", height: "30px" }}
+      >
+        <ArrowBackIcon />
+      </IconButton>
+      <Avatar
+        sx={{
+          width: "30px",
+          height: "30px",
+          color: "#fff",
+          backgroundColor: "#000",
+          border: "2px solid #fff",
+        }}
+      />
+    </Box>
+  );
+};
 
 export default Header;
