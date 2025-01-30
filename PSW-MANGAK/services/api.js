c837eb32dee639290469eca5d31bef7c1a27de6b
João Vaccari <jvvaccari1@gmail.com>
2025-01-30 18:50:59 -0300

    ajustando api

--- PSW-MANGAK/services/api.js
+++ PSW-MANGAK/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5501";
+const API_URL = "http://localhost:55012";
 const axiosInstance = axios.create({
   baseURL: API_URL,
   timeout: 10000,
