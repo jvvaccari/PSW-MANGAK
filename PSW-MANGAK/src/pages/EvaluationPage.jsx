ae0e7450635e42daeeda0464a872f381a5240b12
João Vaccari <jvvaccari1@gmail.com>
2025-01-30 22:17:21 -0300

    ajustando logica de evaluation.js

--- PSW-MANGAK/src/pages/EvaluationPage.jsx
+++ PSW-MANGAK/src/pages/EvaluationPage.jsx
@@ -1,134 +1,110 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import {
-  Box,
-  Typography,
-  TextField,
-  Button,
-  Rating,
-  Alert,
-  CircularProgress,
-  Container,
-} from "@mui/material";
+import { Box, Typography, TextField, Button, Rating, Alert, CircularProgress, Container } from "@mui/material";
 import Navbar from "../components/Navbar";
-import { MangaAPI, EvaluationAPI } from "../../services/api";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchEvaluationsThunk, createEvaluationThunk, updateEvaluationThunk, deleteEvaluationThunk } from "../redux/evaluationSlice";
 
 const EvaluationPage = () => {
   const { mangaId, userId } = useParams();
-  const [manga, setManga] = useState(null);
-  const [comments, setComments] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const dispatch = useDispatch();
+
+  const { evaluations, loading, error } = useSelector((state) => state.evaluations);
   const [rating, setRating] = useState(0);
   const [newComment, setNewComment] = useState("");
   const [editingId, setEditingId] = useState(null);
   const [averageRating, setAverageRating] = useState(0);
 
+  // Carrega as avaliações ao montar o componente
   useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      setError(null);
-      try {
-        const mangaData = await MangaAPI.fetchById(mangaId);
-        const evaluations =
-          await EvaluationAPI.fetchMangaAndEvaluations(mangaId);
-        setManga(mangaData);
-        setComments(evaluations);
-      } catch (err) {
-        console.error("Erro ao carregar dados:", err);
-        setError("Erro ao carregar dados. Tente novamente mais tarde.");
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, [mangaId]);
+    dispatch(fetchEvaluationsThunk(mangaId));
+  }, [dispatch, mangaId]);
 
+  // Atualiza a média das avaliações
   useEffect(() => {
-    if (comments.length > 0) {
-      const avg =
-        comments.reduce((acc, cur) => acc + cur.rating, 0) / comments.length;
+    if (evaluations.length > 0) {
+      const avg = evaluations.reduce((acc, cur) => acc + cur.rating, 0) / evaluations.length;
       setAverageRating(Number(avg.toFixed(1)));
     } else {
       setAverageRating(0);
     }
-  }, [comments]);
+  }, [evaluations]);
 
+  // Função para criar ou atualizar uma avaliação
   const handleSubmit = async () => {
-    if (!userId) {
-      setError("Você precisa estar logado para avaliar um mangá.");
+    if (!userId || rating === 0 || newComment.trim() === "") {
       return;
     }
 
     try {
       if (editingId) {
-        await EvaluationAPI.update(editingId, { rating, comment: newComment });
+        await dispatch(updateEvaluationThunk({ 
+          evaluationId: editingId, 
+          evaluationData: { rating, comment: newComment, userId, username: "Nome do Usuário" }, // Passa o nome do usuário
+          mangaId 
+        }));        
       } else {
-        await EvaluationAPI.createEvaluation(
-          mangaId,
-          { rating, comment: newComment },
-          userId
-        );
+        await dispatch(createEvaluationThunk({ 
+          mangaId, 
+          evaluationData: { rating, comment: newComment }, 
+          userId 
+        }));
       }
+
+      // Limpar estado após criar ou atualizar
       setEditingId(null);
       setNewComment("");
       setRating(0);
-      setError(null);
 
-      const evaluations = await EvaluationAPI.fetchMangaAndEvaluations(mangaId);
-      setComments(evaluations);
+      // Recarrega as avaliações após a operação
+      dispatch(fetchEvaluationsThunk(mangaId));
     } catch (err) {
-      setError("Erro ao salvar avaliação. Tente novamente.", err);
+      console.error("Erro ao salvar avaliação.", err);
     }
   };
 
+  // Função para excluir uma avaliação
   const handleDelete = async (evaluationId) => {
-    try {
-      console.log(evaluationId);
-      const evaluation = comments.find((c) => c.id === evaluationId);
-      if (!evaluation || evaluation.userId !== userId) {
-        setError("Você não pode excluir avaliações de outros usuários.");
-        return;
-      }
-      
-      await EvaluationAPI.delete(evaluationId);
-      
-      setComments((prev) => prev.filter((c) => c.id !== evaluationId));
-      
-    } catch (err) {
-      console.error('Erro ao excluir:', err); 
-      setError("Erro ao excluir avaliação. Tente novamente.");
+    const evaluation = evaluations.find((c) => c.id === evaluationId);
+    if (!evaluation || evaluation.userId !== userId) {
+      return;
     }
-  };  
+
+    try {
+      await dispatch(deleteEvaluationThunk({ evaluationId, mangaId }));
+
+      // Recarrega as avaliações após a exclusão
+      dispatch(fetchEvaluationsThunk(mangaId));
+    } catch (err) {
+      console.error("Erro ao excluir avaliação.", err);
+    }
+  };
+
+  // Função para editar uma avaliação
+  const handleEdit = (comment) => {
+    setEditingId(comment.id);
+    setNewComment(comment.comment);
+    setRating(comment.rating);
+  };
 
   if (loading) return <CircularProgress />;
   if (error) return <Alert severity="error">{error}</Alert>;
-  if (!manga) return <Alert severity="warning">Mangá não encontrado.</Alert>;
 
   return (
     <Box sx={{ backgroundColor: "black" }}>
       <Navbar />
       <Container>
         <Typography variant="h2" color="white" sx={{ marginBottom: 2 }}>
-          {manga.title}
+          {evaluations[0]?.mangaTitle}
         </Typography>
         <Typography variant="h4" color="red" sx={{ marginBottom: 4 }}>
-          {manga.authorId.name}
+          {evaluations[0]?.authorName}
         </Typography>
         <Typography variant="h6" color="white">
           Média de Avaliações: {averageRating} ⭐
         </Typography>
-        {comments.map((comment) => (
-          <Box
-            key={comment.id}
-            sx={{
-              margin: 2,
-              padding: 2,
-              border: "1px solid #ccc",
-              borderRadius: 2,
-              backgroundColor: "gray",
-            }}
-          >
+        {evaluations.map((comment) => (
+          <Box key={comment.id} sx={{ margin: 2, padding: 2, border: "1px solid #ccc", borderRadius: 2, backgroundColor: "gray" }}>
             <Typography variant="h6" color="white">
               {comment.username}
             </Typography>
@@ -136,26 +112,28 @@ const EvaluationPage = () => {
               value={comment.rating}
               readOnly
               sx={{
-                color: "yellow",
+                color: "yellow",  // Cor das estrelas
                 "& .MuiRating-iconFilled": {
-                  color: "yellow",
+                  color: "yellow", // Estrela preenchida em amarelo
                 },
                 "& .MuiRating-iconEmpty": {
-                  color: "yellow",
-                  borderColor: "yellow",
+                  color: "yellow", // Estrela vazia em amarelo
+                },
+                "& .MuiRating-icon:hover": {
+                  borderColor: "yellow", // Borda amarela ao passar o mouse
                 },
               }}
             />
             <Typography color="white">{comment.comment}</Typography>
             {comment.userId === userId && (
-              <Button
-                color="error"
-                variant="outlined"
-                onClick={() => handleDelete(comment.id)}
-                sx={{ marginTop: 2 }}
-              >
-                Excluir
-              </Button>
+              <Box sx={{display: "flex",gap: 1}}>
+                <Button color="error" variant="contained" onClick={() => handleEdit(comment)} sx={{ marginTop: 2 }}>
+                  Editar
+                </Button>
+                <Button color="error" variant="contained" onClick={() => handleDelete(comment.id)} sx={{ marginTop: 2 }}>
+                  Excluir
+                </Button>
+              </Box>
             )}
           </Box>
         ))}
@@ -167,33 +145,41 @@ const EvaluationPage = () => {
           <Rating
             sx={{
               marginY: 1,
-              color: "yellow",
+              color: "yellow",  // Cor das estrelas
               "& .MuiRating-iconFilled": {
-                color: "yellow",
+                color: "yellow", // Estrela preenchida em amarelo
               },
               "& .MuiRating-iconEmpty": {
-                color: "yellow",
-                borderColor: "yellow",
+                color: "yellow", // Estrela vazia em amarelo
+              },
+              "& .MuiRating-icon:hover": {
+                borderColor: "yellow", // Borda amarela ao passar o mouse
               },
             }}
             value={rating}
             onChange={(e, newValue) => setRating(newValue)}
           />
-
           <TextField
             fullWidth
             multiline
             value={newComment}
             onChange={(e) => setNewComment(e.target.value)}
             placeholder="Escreva sua avaliação..."
-            sx={{ marginTop: 1, border: "1px solid #fff" }}
+            sx={{
+              marginTop: 1,
+              border: "1px solid #fff",
+            }}
             InputProps={{ style: { color: "white", fontSize: "18px" } }}
             InputLabelProps={{ style: { color: "white", fontSize: "18px" } }}
           />
           <Button
             variant="contained"
             onClick={handleSubmit}
-            sx={{ marginTop: 3, backgroundColor: "red" }}
+            sx={{
+              marginTop: 3,
+              backgroundColor: "red",
+            }}
+            disabled={!rating || !newComment}
           >
             ENVIAR
           </Button>
